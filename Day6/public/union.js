46451cead446023fa9e06617801001895eced21d
George_Chakama <gtchakama@gmail.com>
2021-04-15 21:29:00 +0200

    union types

---
+++ Day6/public/union.js
@@ -0,0 +1,12 @@
+"use strict";
+function printId(id) {
+    if (typeof id === "string") {
+        // In this branch, id is of type 'string'
+        console.log(id.toUpperCase());
+    }
+    else {
+        // Here, id is of type 'number'
+        console.log(id);
+    }
+}
+printId('Twenty');
