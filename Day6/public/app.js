039aff6b2de18449f3c76c9e75cd11a655759974
George_Chakama <gtchakama@gmail.com>
2021-04-15 12:07:44 +0200

    update

--- Day6/public/app.js
+++ Day6/public/app.js
@@ -1,7 +1,18 @@
 "use strict";
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+;
 var human = {
     name: "George ",
     age: 22,
     hobbies: ['Sports', 'Cooking', 'Programming'],
-    role: [2, 'Actor']
+    role: 'ADMIN'
 };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+console.log(Role);
--- Day6/src/app.ts
+++ Day6/src/app.ts
@@ -1,6 +1,14 @@
+enum Role{ADMIN, READ_ONLY, AUTHOR };
+
 let human = {
     name: "George ",
     age : 22,
     hobbies: ['Sports','Cooking','Programming'],
-    role: [2,'Actor']
+    role: 'ADMIN'
 };
+// const ADMIN = 0;
+// const READ_ONLY = 1;
+// const AUTHOR = 2;
+
+
+console.log(Role);
