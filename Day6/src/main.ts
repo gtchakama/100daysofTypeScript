e37118910d1a81d80781797c20c4af77cbb5cc62
George_Chakama <gtchakama@gmail.com>
2021-04-15 10:27:00 +0200

    Tuple's

--- Day6/src/main.ts
+++ Day6/src/main.ts
@@ -11,17 +11,18 @@ let person: {
     name : string,
     age : number,
     hobbies: string[],
-    role : [number, string]
+    role : [number, string] //Tuple - tells TS that you wantg a special array with 2 dif data types
 } = {
     name: "George ",
     age : 22,
-    hobbies: ['Sports','Cooking'],
+    hobbies: ['Sports','Cooking','Programming'],
     role: [2,'Actor']
 };
 
 person.hobbies.push('Music');
-
-console.log(person);
+person.role.push('Hero');
+person.role.push(4)
+console.log(person.role);
 
 
 // for (const hobby of person.hobbies) {
