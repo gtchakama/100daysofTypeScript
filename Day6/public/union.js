d68d698c3f21bae603e11cffdd80c6c4fb964282
George_Chakama <gtchakama@gmail.com>
2021-04-16 03:47:57 +0200

    Union

--- Day6/public/union.js
+++ Day6/public/union.js
@@ -10,3 +10,15 @@ function printId(id) {
     }
 }
 printId('Twenty');
+function combine(input1, input2) {
+    var result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+console.log(combine(2, 4));
+console.log(combine('George ', "Chakama"));
--- Day6/src/union.ts
+++ Day6/src/union.ts
@@ -9,3 +9,18 @@ function printId(id: number | string) {
     }
 
 printId('Twenty');
+
+function combine(input1:number | string,input2:number | string) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number') {
+        result = input1 + input2;
+    }else{
+        result = input1.toString() + input2.toString()
+    }
+    return result;
+}
+
+console.log(combine(2,4));
+console.log(combine('George ',"Chakama"));
+
+
