1979083229d432371060f4dc1e215638260c512b
George_Chakama <gtchakama@gmail.com>
2021-04-19 15:40:15 +0200

    Day 10 Project Start

--- Day9-Project/public/app1.js
+++ Day9-Project/public/app1.js
@@ -14,5 +14,5 @@ var details = document.querySelector('#details');
 var amount = document.querySelector('#amount');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    console.log(type.value, " from", tofrom.value, details.value, amount.valueAsNumber);
+    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
