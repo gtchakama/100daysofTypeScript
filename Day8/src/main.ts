55ac4e327bb6c7b9091486cc5d4de30957661b44
George_Chakama <gtchakama@gmail.com>
2021-04-19 12:19:38 +0200

    reg update

--- Day8/src/main.ts
+++ Day8/src/main.ts
@@ -1,12 +1,12 @@
-console.log(greetNamed("John"));
-console.log(greetUnnamed("John"));
-function greetNamed(name : string) : string {
-if(name) {
-return "Hi! " + name;
-}
-}
-var greetUnnamed = function(name : string) : string {
-if(name){
-return "Hi! " + name;
-}
-}
+// console.log(greetNamed("John"));
+// console.log(greetUnnamed(name:string = "John"));
+// function greetNamed(name : string) : string {
+// if(name) {
+// return "Hi! " + name;
+// }
+// }
+// var greetUnnamed = function(name : string) : string {
+// if(name){
+// return "Hi! " + name;
+// }
+// }
--- Day9-Project/src/app.ts
+++ Day9-Project/src/app.ts
@@ -1,5 +1,4 @@
 const anchor = document.querySelector('a')!;
-if (anchor) {
-    console.log(anchor.href);
-    
-}
+
+console.log(anchor.innerHTML);
+
