a653bde980f44cb7ebb8dcedde3c6745a881fd11
George_Chakama <gtchakama@gmail.com>
2021-04-27 15:14:13 +0200

    using types

---
+++ Day14-c/app.js
@@ -0,0 +1,5 @@
+console.log("Hey lets go");
+function add(n1, n2) {
+    return n1 + n2;
+}
+console.log(add(3, 5));
--- Day14-c/app.ts
+++ Day14-c/app.ts
@@ -1,1 +1,6 @@
-console.log("Hey");
+console.log("Hey lets go");
+
+function add(n1:number, n2:number) {
+    return n1 + n2;
+}
+console.log(add(3,5));
