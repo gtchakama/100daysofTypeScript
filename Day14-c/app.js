42fe057d4c7359eecf560aed177b53d5ccc7a909
George_Chakama <gtchakama@gmail.com>
2021-04-27 15:37:56 +0200

    creating ts config for better compiling

--- Day14-c/app.js
+++ Day14-c/app.js
@@ -3,3 +3,6 @@ function add(n1, n2) {
     return n1 + n2;
 }
 console.log(add(3, 5));
+var meal;
+meal = ['wheat'];
+console.log(typeof meal);
--- Day14-c/app.ts
+++ Day14-c/app.ts
@@ -4,3 +4,9 @@ function add(n1:number, n2:number) {
     return n1 + n2;
 }
 console.log(add(3,5));
+
+let meal : number | string;
+meal = ['wheat',4];
+
+console.log(typeof meal);
+
