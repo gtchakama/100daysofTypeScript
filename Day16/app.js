f6ada66504f56a2ab54bd4b90d86847a3800e40e
George_Chakama <gtchakama@gmail.com>
2021-04-29 20:05:43 +0200

    Working with Tuples
    A tuple type is another sort of Array type that knows exactly how many elements it contains, and
    exactly which types it contains at specific positions.
    
    type StringNumberPair = [string, number];

--- Day16/app.js
+++ Day16/app.js
@@ -9,12 +9,16 @@
 var person = {
     name: 'George',
     age: 22,
-    hobbies: ['coding', 'jogging', 'music']
+    hobbies: ['coding', 'jogging', 'music'],
+    role: [2, 'author'] //union type
 };
+person.role.push('Runner');
+person.role[0] = 40;
 var favActivities;
 favActivities = ['sports'];
-console.log(person);
+// console.log(person);
 // console.log(person.age);
-person.hobbies.forEach(function (hobby) {
-    console.log(hobby.toUpperCase());
-});
+// person.hobbies.forEach(hobby => {
+//     console.log(hobby.toUpperCase());    
+// });
+console.log(person.role);
--- Day16/app.ts
+++ Day16/app.ts
@@ -5,19 +5,30 @@
 //     name: 'George',
 //     age: 22
 // };
-const person =  {
+const person: {
+    name: string,
+    age:number,
+    hobbies:string[],
+    role:[number,string]
+} =  {
     name: 'George',
     age: 22,
-    hobbies: ['coding','jogging','music']
+    hobbies: ['coding','jogging','music'],
+    role: [2,'author'] //union type
 };
+
+person.role.push('Runner');
+person.role[0] = 40;
 
 let favActivities: string [];
 favActivities = ['sports'];
 
 
-console.log(person);
+// console.log(person);
 // console.log(person.age);
 
-person.hobbies.forEach(hobby => {
-    console.log(hobby.toUpperCase());    
-});
+// person.hobbies.forEach(hobby => {
+//     console.log(hobby.toUpperCase());    
+// });
+
+console.log(person.role);
