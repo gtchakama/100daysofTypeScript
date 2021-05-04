4ee433c2963e1c8d4918bc7b4846b855ac728994
George_Chakama <gtchakama@gmail.com>
2021-05-04 16:35:09 +0200

    Functions & Types

--- Day18/app.ts
+++ Day18/app.ts
@@ -11,3 +11,7 @@ let combinedValue: Function;
 combinedValue = add;
 console.log(combinedValue(8,7));
 
+function addAndHandle(n1:number, n2:number, cb:(num:number)) {
+    const result = n1 + n2;
+    cb(result);
+}
