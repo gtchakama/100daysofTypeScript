dbe4e35534be90592e75138d9668ebbd3cfc7796
George_Chakama <gtchakama@gmail.com>
2021-05-05 12:04:12 +0200

    Never Type | Day 20

---
+++ Day20/app.js
@@ -0,0 +1,14 @@
+"use strict";
+var userInput;
+var userName;
+userInput = 5;
+userInput = 'Food';
+if (typeof userInput === 'string') {
+    userName = userInput;
+}
+function generateError(message, code) {
+    throw { message: message, ErrorCode: code };
+}
+// const result = generateError('An error occured!',500);
+// console.log(result);
+generateError('An error occured!', 500);
